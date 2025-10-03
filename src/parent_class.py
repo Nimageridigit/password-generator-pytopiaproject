d61f5b1ff058efc9cf91f748983e8e967ded7d5a
Nima R.Sureh <nimageridi@gmail.com>
2025-10-03 17:21:53 +0330

    parent class updated

---
+++ src/parent_class.py
@@ -0,0 +1,15 @@
+from abc import ABC , abstractmethod
+
+class PasswordGenerator(ABC):
+    """
+    Base class for generating passwords.
+    """
+    @abstractmethod
+    def password_generator(self) -> str:
+        """
+        Every subclasses need to override this method to generate password.
+        """
+        pass
+
+
+
---
+++ src/subclass_random.py
@@ -0,0 +1,7 @@
+from parent_class import *
+
+class Random_pass(PasswordGenerator):
+    def __init__(self, lengh: int=8, include_number: bool= False, include_symbol: bool= False ):
+        
+    def password_generator(self) -> str:
+        return super().password_generator()
