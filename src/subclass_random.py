5212104467ff475f7e67718a935a69d5e1da36e0
Nima R.Sureh <nimageridi@gmail.com>
2025-10-04 15:00:32 +0330

    Random password generator created...

--- src/subclass_random.py
+++ src/subclass_random.py
@@ -1,7 +1,19 @@
+import random
+import string
+
 from parent_class import *
 
 class Random_pass(PasswordGenerator):
-    def __init__(self, lengh: int=8, include_number: bool= False, include_symbol: bool= False ):
-        
+    def __init__(self, length: int=8, include_number: bool= False, include_symbol: bool= False ):
+        self.length = length
+        self.include_number = include_number
+        self.include_symbol = include_symbol
     def password_generator(self) -> str:
-        return super().password_generator()
+        chars = string.ascii_letters
+        if self.include_number:
+            chars += string.digits
+        if self.include_symbol:
+            chars += string.punctuation
+        return ''.join(random.choice(chars) for _ in range(self.length))
+# test= Random_pass()
+# print(test.password_generator())
